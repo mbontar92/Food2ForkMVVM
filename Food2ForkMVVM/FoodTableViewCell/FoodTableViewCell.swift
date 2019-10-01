eedda91e0b115e3966ba6132d6fa37b1afef5d5e
mbontar92 <bontar92@gmail.com>
2019-10-01 09:35:37 +0300

    create project

---
+++ Food2ForkMVVM/FoodTableViewCell/FoodTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodTableViewCell.swift
+//  Food2ForkMVVM
+//
+//  Created by Lorem on 9/30/19.
+//  Copyright © 2019 Bontar Mykhailo. All rights reserved.
+//
+
+import UIKit
+
+class FoodTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Food2ForkMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food2ForkMVVM
+//
+//  Created by Lorem on 9/30/19.
+//  Copyright © 2019 Bontar Mykhailo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
